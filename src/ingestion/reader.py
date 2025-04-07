2cf1a1ab443cf6e74e17cf401e07bb89a39c3645
JUNJIE WU <junjie.wu@sdggroup.com>
2025-04-07 11:38:46 +0200

    fix: edited for NDJSON file

--- src/ingestion/reader.py
+++ src/ingestion/reader.py
@@ -1,5 +1,3 @@
-# File: src/ingestion/reader.py
-
 from src.utils.logger import get_logger
 
 logger = get_logger()
@@ -10,8 +8,4 @@ class DataReader:
 
     def read_source(self, source_conf):
         logger.info(f"Leyendo datos desde {source_conf['path']} con formato {source_conf['format']}")
-        if source_conf["format"].lower() == "json":
-            # Usa multiLine true para archivos JSON que son un array en lugar de JSON Lines.
-            return self.spark.read.option("multiLine", "true").json(source_conf["path"])
-        else:
-            return self.spark.read.format(source_conf["format"]).json(source_conf["path"])
+        return self.spark.read.format(source_conf["format"]).json(source_conf["path"])
